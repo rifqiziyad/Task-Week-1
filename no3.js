abbca9e8bd1f8819515f7462e209fc8c4d11846b
rifqiziyad <72638066+rifqiziyad@users.noreply.github.com>
2021-03-21 21:46:55 +0700

    Update no3.js

--- no3.js
+++ no3.js
@@ -1,11 +1,17 @@
-const printSegitiga = 5;
-let hasil = '';
+const printSegitiga = '5'
+let hasil = ''
 
-for(let baris = printSegitiga; baris > 0; baris--) {
-    for(let angka = 1; angka <= baris; angka++ ) {
-        hasil += angka;
+if (typeof printSegitiga != "number") {
+    console.log('Data harus number')
+} else {
+    for(let baris = printSegitiga; baris > 0; baris--) {
+        for(let angka = 1; angka <= baris; angka++ ) {
+            hasil += angka
+        }
+        hasil += '\n'
     }
-    hasil += '\n';
-}
 console.log(hasil)
+}
+
+
 
